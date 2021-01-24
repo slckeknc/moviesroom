4d457bdcc9812d9f83cd3cc648a0552c8ca834f6
Abdurrahman Ekinci <slckeknc@gmail.com>
2021-01-24 19:26:41 +0100

    layout and seo component filename change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import {
   Wrapper,
   Image,
